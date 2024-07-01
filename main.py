b68a4fcdc01898d03bb000b4168d31cc56897f45
kaufman79 <kaufman79@gmail.com>
2024-07-01 08:48:33 -0400

    stores upcoming tour and dates in txt file, runs email function (currently empty)

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import requests
 import selectorlib
 
-
 URL = "http://programmer100.pythonanywhere.com/tours/"
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
 
 def scrape(url):
     """scrape the page source (html of the page) from the URL"""
@@ -12,13 +12,32 @@ def scrape(url):
     source = response.text
     return source
 
+
 def extract(source):
     extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
-    value = extractor.extract(source)["tours"] 
+    value = extractor.extract(source)["tours"]
     return value
 
+
+def send_email():
+    print("Email was sent")
+
+def store(extracted):
+    """appends extracted tour to txt file"""
+    with open('data.txt', 'a') as file:
+        file.write(extracted + "\n")
+
+
+def read(extracted):
+    with open("data.txt", 'r') as file:
+        return file.read()
 
 if __name__ == "__main__":
     scraped = scrape(URL)
     extracted = extract(scraped)
     print(extracted)
+    content = read(extracted)
+    if extracted != "No upcoming tours":
+        if extracted not in content:
+            store(extracted)
+            send_email()
