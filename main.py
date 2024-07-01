b1baadb33c1c1bd72ffa0526bd70b2dc9c329f5a
kaufman79 <kaufman79@gmail.com>
2024-07-01 09:30:49 -0400

    runs continuously (every 200 seconds) until stopped

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import requests
 import selectorlib
+import smtplib, ssl
+import os
+import time
 
 URL = "http://programmer100.pythonanywhere.com/tours/"
 HEADERS = {
@@ -19,8 +22,19 @@ def extract(source):
     return value
 
 
-def send_email():
+def send_email(message):
+    password = os.getenv("GOOGLEPASSWORD")  # need to first input environmental variable in os
+    username = "ryankaufman79@gmail.com"
+    receiver = "ryankaufman79@gmail.com"
+    host = "smtp.gmail.com"
+    port = 465
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
     print("Email was sent")
+
 
 def store(extracted):
     """appends extracted tour to txt file"""
@@ -32,12 +46,19 @@ def read(extracted):
     with open("data.txt", 'r') as file:
         return file.read()
 
+
+subject = ("\
+Subject: New music event")
+
 if __name__ == "__main__":
-    scraped = scrape(URL)
-    extracted = extract(scraped)
-    print(extracted)
-    content = read(extracted)
-    if extracted != "No upcoming tours":
-        if extracted not in content:
-            store(extracted)
-            send_email()
+    while True:
+        scraped = scrape(URL)
+        extracted = extract(scraped)
+        print(extracted)
+
+        content = read(extracted)
+        if extracted != "No upcoming tours":
+            if extracted not in content:
+                store(extracted)
+                send_email(message=subject + "\n\nHey, New Event was found")
+        time.sleep(200)
